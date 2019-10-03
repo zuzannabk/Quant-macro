da1dfae7cb3c4eb1fbb6b547e41e2ed20cf24a42
zuzannabk <32644013+zuzannabk@users.noreply.github.com>
2019-10-04 00:13:24 +0200

    Add files via upload

--- Homework_2/question2.py
+++ Homework_2/question2.py
@@ -7,14 +7,19 @@ Created on Mon Sep 30 02:05:32 2019
 
 
 import numpy as np
+import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from mpl_toolkits import mplot3d
+#tt=36
 
 alpha = 0.5
 sigma = 0.25
 nods = 20
 N = [3, 9, 15]
+perc_ind = [5, 10, 25,50, 75, 90, 95]
+perc_ind_full = np.linspace(1, 100, 100)
 
 def f(k, h):
     y = ((1-alpha)*k**((sigma-1)/sigma)+alpha*h**((sigma-1)/sigma))**(sigma/(sigma-1))
@@ -65,6 +70,23 @@ surf = ax.plot_surface(k, h, real_val, alpha = 0.5, cmap=cm.bwr, linewidth=0, an
 fig.colorbar(surf, shrink=1, aspect=10)
 plt.title('Real value')
 plt.show()
+#plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
+#tt+=1
+plt.clf()
+
+
+##real iso
+perc_true_full = []
+for i in perc_ind_full:
+    perc_true_full.append(np.percentile(real_val, i))
+perc_true = pd.Series(perc_true_full).iloc[perc_ind]
+
+fig, ax = plt.subplots()
+CS = ax.contour(k, h, real_val, perc_true)
+ax.clabel(CS, inline=1, fontsize=10)
+plt.title('Real value - percentiles')
+plt.show()
+#plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
 plt.clf()
 
 for n in N:
@@ -79,6 +101,8 @@ for n in N:
     fig.colorbar(surf, shrink=1, aspect=10)
     plt.title('Chebyshev approximation of degree ' +str(n))
     plt.show()
+    #plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
+    #tt+=1
     plt.clf()
     
     fig = plt.figure()
@@ -87,7 +111,37 @@ for n in N:
     fig.colorbar(surf, shrink=1, aspect=10)
     plt.title('Chebyshev error of degree ' +str(n))
     plt.show()
+    #plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
+    #tt+=1
     plt.clf()
+    
+## aprox iso 
+    perc_approx_full = []
+    for i in perc_ind_full:
+        perc_approx_full.append(np.percentile(fit, i))
+    perc_approx = pd.Series(perc_approx_full).iloc[perc_ind]
+        
+    fig, ax = plt.subplots()
+    CS = ax.contour(k, h, fit, perc_approx)
+    ax.clabel(CS, inline=1, fontsize=10)
+    plt.title('Approximation of degree ' + str(n) + ' - percentiles')
+    plt.show()
+    #plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
+    #tt+=1
+    plt.clf()
+
+    iso_error = np.array(perc_true_full)-np.array(perc_approx_full)
+    plt.plot(perc_ind_full, iso_error)
+    plt.xlabel('percentile')
+    plt.ylabel('error')
+    plt.title('Differents between real and approximation (deg.'+str(n)+') value for given percentiles')
+    plt.show()
+    #plt.savefig('screenshot0'+str(tt)+'.png', bbox_inches='tight')
+    #tt+=1
+    plt.clf()
+
+        
+        
     
     
     
